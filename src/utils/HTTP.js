ccdcf3d4cf1ce005b856982e4b56a220215bd177
theobalzeau <theo.ba73@gmail.com>
2022-04-04 17:32:27 +0200

    Update HTTP.js

--- src/utils/HTTP.js
+++ src/utils/HTTP.js
@@ -8,7 +8,6 @@ export default class HTTP {
             headers: { 'Content-Type': 'application/json'},
             body: JSON.stringify(params)
         };
-        fetch(url, requestOptions)
             .then(function(response){
                 return response.json()
             })
