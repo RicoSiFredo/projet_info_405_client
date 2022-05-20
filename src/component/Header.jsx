f4961a295c18a5032cce6b741d48f6f54f5eb685
RicoSiFredo <ericbarnierchx@gmail.com>
2022-05-20 22:03:58 +0200

    home css

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -129,7 +129,7 @@ function Header({user, navigate, search, updateSearch, notif, updateNotif}){
 
     return (
         <nav className="navbar navbar-dark bg-dark justify-content-between">            <button className='ms-3 btn btn-primary me-3' onClick={back}>Retour</button>
-            <Link to={"/"}>
+            <Link className="text-decoration-none" to={"/"}>
                 <div className="navbar-brand ps-2 click">
                     <img src={Constant.BASE_IMAGE + "logo_usmb.png"} width="30" height="30" className="align-top" alt=""/>
                     <h4 className="p-3 d-inline">Projet 405</h4>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,48 +2,55 @@ import { Button, Carousel } from "react-bootstrap";
 import PageEnum from "../enum/PageEnum";
 import React from "react"
 import { Link } from "react-router-dom";
-import Holder from 'react-holder';
 import Constant from "../utils/Constant";
 
 function Home({user, navigate}){
-    return <div className="d-flex justify-content-around">
+    return <div>
+            <div className="d-flex justify-content-around">
             <div className="card mt-4 me-5 ms-5 w-100">
                 <Carousel variant="dark">
                     <Carousel.Item>
-                        <img
-                            className="d-block w-100"
-                            src={"http://os-vps418.infomaniak.ch:1187/l2_info_9_file/image/91bc2cf3-a45d-4bb7-9f61-fbe3501d88b2.png"}
+                        <img 
+                            className="homeBanner"
+                            src={"http://os-vps418.infomaniak.ch:1187/l2_info_9_file/image/4632d829-5da9-456b-8f81-aaa50b75e52e.jpg"}
                             alt="First slide"
                         />
+                            <div class="centered"><p className="titleOverImg">Bienvenue</p></div>
                         <Carousel.Caption>
-                            <h5>Pour commencer créer vous un compte</h5>
-                            <p>Personnalisez votre profil pour être le plus attractif possible<br></br>Montrez vos compétences</p>
+                            <h4>Pour commencer créer vous un compte</h4>
+                            <h6>Personnalisez votre profil pour être le plus attractif possible<br></br>Montrez vos compétences</h6>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
-                            className="d-block w-100"
-                            src={"http://os-vps418.infomaniak.ch:1187/l2_info_9_file/image/91bc2cf3-a45d-4bb7-9f61-fbe3501d88b2.png"}
+                            className="homeBanner"
+                            src={"http://os-vps418.infomaniak.ch:1187/l2_info_9_file/image/1f38497b-9c84-4da3-8cc2-a73661224da6.png"}
                             alt="Second slide"
                         />
+                            <div class="centered"><p className="titleOverImg">Votre projet</p></div>
                         <Carousel.Caption>
-                            <h5>Créer un projet</h5>
-                            <p>Concevez et publiez votre projet pour être rejoins par des professionnels</p>
+                            <h4>Créer un projet</h4>
+                            <h6>Concevez et publiez votre projet pour être rejoins par des professionnels</h6>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
-                            className="d-block w-100"
-                            src={"http://os-vps418.infomaniak.ch:1187/l2_info_9_file/image/91bc2cf3-a45d-4bb7-9f61-fbe3501d88b2.png"}
+                            className="homeBanner"
+                            src={"http://os-vps418.infomaniak.ch:1187/l2_info_9_file/image/5671235c-6421-4450-bd32-92fba7496448.png"}
                             alt="Third slide"
                         />
+                            <div class="centered"><p className="titleOverImg">Postuler !</p></div>
                         <Carousel.Caption>
-                            <h5>Proposez-vous dans un projet</h5>
-                            <p>Si votre profil interesse le chef de projet, vous ferez partie de la team</p>
+                            <h4>Proposez-vous dans un projet</h4>
+                            <h6>Si votre profil interesse le chef de projet, vous ferez partie de la team</h6>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
+        </div>
+        <div className="card m-5">
+            <h2>ICI METTRE LISTE DE PROJET</h2>
+        </div>
         </div>
 }
 export default Home;
